0e326112d7c0d9539ec2dfa38917dcd636938f94
RFButler <117391840+RFButler@users.noreply.github.com>
2023-01-25 15:28:18 +0000

    Add files via upload

---
+++ Hello/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //quick program for book exercise
+        System.out.println("Hello World");
+        System.out.println("How are things?");
+    }
+}
